04f6f7abe147d51ba03210ef84a936582a6420c8
Subash Xess <subuxess@gmail.com>
2023-01-24 01:18:43 +0530

    day-4

--- lib/screens/tutor_profile_page/tutor_profile_page.dart
+++ lib/screens/tutor_profile_page/tutor_profile_page.dart
@@ -10,76 +10,58 @@ class TutorProfilePage extends StatefulWidget {
 
 class _TutorProfilePageState extends State<TutorProfilePage>
     with TickerProviderStateMixin {
-  late ScrollController _scrollController;
-
-  final List<String> listItems = [];
-
-  final List<String> _tabs = <String>[
-    "Details",
-    "Course",
-    "Review",
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController();
-  }
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: DefaultTabController(
-        length: _tabs.length,
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              SliverOverlapAbsorber(
-                handle:
-                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                sliver: SliverAppBar(
-                  // expandedHeight: size.height * 0.4,
+      body: SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: NestedScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
                   backgroundColor: Colors.deepPurple.shade200,
-                  floating: true,
-                  pinned: true,
-                  snap: false,
-                  primary: true,
-                  forceElevated: innerBoxIsScrolled,
-                  bottom: TabBar(
-                    isScrollable: true,
-                    tabs: _tabs.map((String name) => Tab(text: name)).toList(),
+                  collapsedHeight: size.height * 0.30,
+                  expandedHeight: size.height * 0.30,
+                  flexibleSpace: SafeArea(
+                    child: Container(
+                      width: size.width,
+                      height: size.height * 0.26,
+                      margin: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        color: Colors.deepPurple.shade300,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ];
-          },
-          body: TabBarView(
-            // These are the contents of the tab views, below the tabs.
-            children: _tabs.map((String name) {
-              return SafeArea(
-                top: false,
-                bottom: false,
-                child: Builder(
-                  // This Builder is needed to provide a BuildContext that is "inside"
-                  // the NestedScrollView, so that sliverOverlapAbsorberHandleFor() can
-                  // find the NestedScrollView.
-                  builder: (BuildContext context) {
-                    return CustomScrollView(
-                      // the tab view is not on the screen.
-                      key: PageStorageKey<String>(name),
-                      slivers: <Widget>[
-                        SliverOverlapInjector(
-                          handle:
-                              NestedScrollView.sliverOverlapAbsorberHandleFor(
-                                  context),
-                        ),
+                const SliverPersistentHeader(
+                  floating: true,
+                  pinned: true,
+                  delegate: MyDelegate(
+                    TabBar(
+                      indicatorColor: Colors.blue,
+                      unselectedLabelColor: Colors.grey,
+                      labelColor: Colors.black,
+                      tabs: [
+                        Tab(text: 'Details'),
+                        Tab(text: 'Course'),
+                        Tab(text: 'Review'),
                       ],
-                    );
-                  },
+                    ),
+                  ),
                 ),
-              );
-            }).toList(),
+              ];
+            },
+            body: const TabBarView(
+              children: [
+                Text('Details page'),
+                Text('Course page'),
+                Text('Review page'),
+              ],
+            ),
           ),
         ),
       ),
@@ -87,14 +69,25 @@ class _TutorProfilePageState extends State<TutorProfilePage>
   }
 }
 
-// SliverToBoxAdapter(
-//               child: Container(
-//                 width: size.width,
-//                 height: size.height * 0.26,
-//                 margin: const EdgeInsets.all(16.0),
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(12.0),
-//                   color: Colors.deepPurple.shade400,
-//                 ),
-//               ),
-//             ),
+class MyDelegate extends SliverPersistentHeaderDelegate {
+  const MyDelegate(this.tabBar);
+
+  final TabBar tabBar;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(color: Colors.red.shade200, child: tabBar);
+  }
+
+  @override
+  double get maxExtent => tabBar.preferredSize.height;
+
+  @override
+  double get minExtent => tabBar.preferredSize.height;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
